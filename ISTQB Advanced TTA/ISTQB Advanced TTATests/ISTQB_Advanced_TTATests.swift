63f1f58612b96230a66a54ff084f60297b3ce4d4
Ruby Pham <rubypham@352SonTTH-MC.local>
2017-07-20 11:36:15 +0700

    demo.txt

---
+++ ISTQB Advanced TTA/ISTQB Advanced TTATests/ISTQB_Advanced_TTATests.swift
@@ -0,0 +1,36 @@
+//
+//  ISTQB_Advanced_TTATests.swift
+//  ISTQB Advanced TTATests
+//
+//  Created by Ruby Pham on 7/20/17.
+//  Copyright © 2017 Ruby Pham. All rights reserved.
+//
+
+import XCTest
+@testable import ISTQB_Advanced_TTA
+
+class ISTQB_Advanced_TTATests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
